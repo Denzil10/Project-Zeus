d8c56b82a3c0fb4ade996642a1f8d326a1c7fbc6
denzil <2020.denzil.puvathingal@ves.ac.in>
2024-06-28 13:21:25 +0530

    added info endpoint, fixed register, tested checkin

--- server.py
+++ server.py
@@ -2,8 +2,10 @@
 from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, db
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import re
+import secrets
+import string
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("credentials.json") 
@@ -17,8 +19,12 @@ def sanitize_phone(phone):
 
 @app.route('/register', methods=['POST'])
 def register():
-    header = request.headers
-    print(header)
+    headers = request.headers.get('username').split()
+    username = headers[0]
+    referrer_code = headers[1]
+    # Generate a random referral code
+    code_length = 5
+    referral_code = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(code_length))
     # referrer_code = request.headers.get('code')
     data = request.json
     print(data)
@@ -38,6 +44,11 @@ def register():
 
     user_ref = db.reference(f'users/{number}')
     user = user_ref.get()
+    
+    now = datetime.now(timezone.utc)
+    today_date = now.strftime('%Y-%m-%d')
+    yes_time = now - timedelta(days=1)
+    yes_date =yes_time.strftime('%Y-%m-%d')
 
     if not user:
         user = {
@@ -45,22 +56,54 @@ def register():
             'username': username,
             'referrerCode': referrer_code,
             'level': level,
-            'lastMessageDate': None,
+            'lastCheckInDate': yes_date,
+            'referralCount': 0, 
+            'referralCode': referral_code,
+            'streak': 0,
+            'bestStreak': 0
         }
     else:
         user.update({
             'username': username,
             'level': level,
+            'lastCheckInDate': yes_date
         })
 
     user_ref.set(user)
 
-    response_message = f"Welcome {user.get('username', '')}! Upgraded to lvl {user['level']}"
+    response_message = f"🎉 Welcome {user.get('username', '')}!\n Upgraded to lvl {user['level']}🔥"
+    return jsonify({"replies": [{"message": response_message}]}), 200
+
+@app.route('/info', methods=['POST'])
+def info():
+    data = request.json
+    message = data.get('query')
+    if not message:
+        return jsonify({"replies": [{"message": "❌ Invalid message type"}]}), 400
+
+
+   
+    number =  sanitize_phone(message.get('sender')) 
+    user_ref = db.reference(f'users/{number}')
+    user = user_ref.get()
+    
+    info = (
+    "Info😎\n"
+    f"Username: {user['username']}\n"
+    f"Level: {user['level']}\n"
+    f"Streak: {user['streak']}\n"
+    f"Best Streak: {user['bestStreak']}\n"
+    f"Referral Code: {user['referralCode']}\n"
+    f"Referral Count: {user['referralCount']}\n"
+)
+
+    response_message = f"{info}"
     return jsonify({"replies": [{"message": response_message}]}), 200
 
 @app.route('/checkin', methods=['POST'])
 def checkin():
     data = request.json
+    print(data)
 
     if not data:
         app.logger.error("Invalid JSON data received")
@@ -71,25 +114,38 @@ def checkin():
         return jsonify({"replies": [{"message": "❌ Invalid message type"}]}), 400
 
     user_phone = message.get('sender')
-
-    now = datetime.utcnow()
-    today = now.strftime('%Y-%m-%d')
-
-    user_ref = db.reference(f'users/{user_phone}')
+    number = sanitize_phone(user_phone)
+    user_ref = db.reference(f'users/{number}')
     user = user_ref.get()
+
+    now = datetime.now(timezone.utc)
+    today_date = now.strftime('%Y-%m-%d')
+    yes_time = now - timedelta(days=1)
+    yes_date =yes_time.strftime('%Y-%m-%d')
 
     if not user:
         return jsonify({"replies": [{"message": "Please register first"}]}), 404
+    
+    print("userlast " + str(user['lastCheckInDate']) + " today " + str(today_date))
 
-    if user['lastMessageDate'] != today:
-        if user['lastMessageDate'] and user['lastMessageDate'] != today:
-            user['level'] = 0
+    
+    if user['lastCheckInDate'] == today_date:
+        msg = f"✅ Check-in has been already done"
+    elif user['lastCheckInDate'] != yes_date:
+        user['level'] = 1
+        user['streak'] = 1
+        msg = f"🔴 You broke your streak. Starting from lvl 1"
+    else:
         user['level'] += 1
-        user['lastMessageDate'] = today
+        user['lastCheckInDate'] = today_date
+        user['streak'] += 1
+        if user['streak']>user['bestStreak']:
+            user['bestStreak'] = user['streak']
+        msg = f"🎉 Reached Lvl {user['level']}"
 
     user_ref.set(user)
-
-    return jsonify({"replies": [{"message": f"Reached Lvl{user['level']}"}]}), 200
+    # return jsonify({"replies": [{"message": "Please register first"}]}), 404
+    return jsonify({"replies": [{"message": msg}]}), 200
 
 @app.route('/')
 def index():
