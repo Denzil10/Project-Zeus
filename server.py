bb8162df28a4b55af859ccc3041ac81979b34c9d
Omkar <omkar.mahajan872@gmail.com>
2024-06-30 18:47:32 +0530

    deployment version 1.0

--- server.py
+++ server.py
@@ -6,9 +6,25 @@ from datetime import datetime, timezone, timedelta
 import re
 import secrets
 import string
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("credentials.json") 
+firebase_cred = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+cred = credentials.Certificate(firebase_cred)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://project-zeus-98a8c-default-rtdb.firebaseio.com/'  # Replace with your database URL
 })
@@ -23,7 +39,7 @@ def getUser(query):
         
     # if number then clean    
     if user_identifier and not any(char.isdigit() for char in user_identifier):
-        print("It's a saved contact") # might have to remove the space 
+        pass # might have to remove the space 
     else: 
         user_identifier = re.sub(r'[^0-9]', '', user_identifier)
         
